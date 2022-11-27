f1293c60af46ac57249d2524d87e0fabbf089d52
DAQEM <daqemyt@gmail.com>
2022-11-27 12:22:52 +0100

    Initial commit

---
+++ fabric/src/main/java/com/daqem/jobsplus/fabric/ExampleModFabric.java
@@ -0,0 +1,11 @@
+package com.daqem.jobsplus.fabric;
+
+import com.daqem.jobsplus.ExampleMod;
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
---
+++ forge/src/main/java/com/daqem/jobsplus/forge/ExampleModForge.java
@@ -0,0 +1,15 @@
+package com.daqem.jobsplus.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import com.daqem.jobsplus.ExampleMod;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleModForge {
+    public ExampleModForge() {
+        // Submit our event bus to let architectury register our content on the right time
+        EventBuses.registerModEventBus(ExampleMod.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        ExampleMod.init();
+    }
+}
